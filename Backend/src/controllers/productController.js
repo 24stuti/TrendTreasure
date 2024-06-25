5f022a57e8a1b26cada04768f6dbf9c686f6d9b0
Desh Deepak <ddeepak@extcanopyhealth.onmicrosoft.com>
2024-06-25 15:46:58 +0530

    added admin routes

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -36,8 +36,43 @@ const getProductsByCategory = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Add a new product
+// @route   POST /api/products
+// @access  Private/Admin
+const addProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, category, countInStock, image } = req.body;
+
+  const product = new Product({
+    name,
+    price,
+    description,
+    category,
+    countInStock,
+    image
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findOne(req.params.id);
+
+  if (product) {
+    await Product.deleteOne(req.params.id);
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
+});
+
 module.exports = {
   getProducts,
   getProductById,
-  getProductsByCategory
+  getProductsByCategory,
+  addProduct,
+  deleteProduct
 };
--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const { protect, admin } = require('../middleware/authMiddleware');
 const {
   getProducts,
   getProductById,
   getProductsByCategory,
+  addProduct,
+  deleteProduct
 } = require('../controllers/productController');
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
+router.route('/').post(protect, admin, addProduct);
 router.route('/:id').get(getProductById);
+router.route('/:id').delete(protect, admin, deleteProduct);
 router.route('/category/:category').get(getProductsByCategory);
 
 module.exports = router;
